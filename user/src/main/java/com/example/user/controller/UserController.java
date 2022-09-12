20e78346299fffc840cac6f16a6301713bad5eab
Mirae Lee <lmrae0624@gmail.com>
2022-09-12 23:08:53 +0900

    주석 추가

--- user/src/main/java/com/example/user/controller/UserController.java
+++ user/src/main/java/com/example/user/controller/UserController.java
@@ -12,28 +12,33 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    //회원 등록
     @PostMapping("/user")
-    public User registerUser(@RequestBody User user) { //등록
+    public User registerUser(@RequestBody User user) { 
         return userService.registerUser(user);
     }
 
+//회원 조회
     @GetMapping("/user")
-    public List<User> findUserAll(){ //전체 조회
+    public List<User> findUserAll(){ 
         return userService.findUserAll();
     }
 
+//회원 상세 조회
     @GetMapping("/user/{id}")
-    public User findUserOne(@PathVariable("id") String id){ //개별 조회
+    public User findUserOne(@PathVariable("id") String id){ 
         return userService.findUserOne(id);
     }
 
+//회원 삭제
     @DeleteMapping("/user/{id}")
-    public void deleteUser(@PathVariable String id) { //삭제
+    public void deleteUser(@PathVariable String id) { 
         userService.deleteUser(id);
     }
 
+//회원 정보 수정
     @PutMapping("/user/{id}")
-    public User modifyUser(@PathVariable String id, @RequestBody User user) { //수정
+    public User modifyUser(@PathVariable String id, @RequestBody User user) { 
         userService.modifyUser(id, user);
         return userService.findUserOne(id);
     }
